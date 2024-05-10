691783af27884d02d9a2cf144cc19e3bb3290e9f
j <email@gmail.com>
2024-05-10 11:34:42 +0200

    fix: posix generate incidence line

--- userscript.sh
+++ userscript.sh
@@ -65,5 +65,5 @@ while IFS= read -r line; do
 done < "$datafile"
 id=$(expr $id + 1)
 # Guardar los datos en data.txt
-data=";;open;$incidenceType;$email;$desc;$current_month_year;$id"
+data=(";;open;$incidenceType;$email;$desc;$current_month_year;$id")
 echo "$data" >> incidencies.txt
